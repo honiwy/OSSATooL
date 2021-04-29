2470d0c4660935a743a7b9bea28fd2d594eed973
Rachel <rachel.chuang@abilitycorp.com.tw>
2021-04-29 17:31:30 +0800

    Feat - connection and qfil UI

--- ossaTool/MainForm.Designer.cs
+++ ossaTool/MainForm.Designer.cs
@@ -29,20 +29,174 @@ namespace ossaTool
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControlMenu = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pgBarQFIL = new System.Windows.Forms.ProgressBar();
+            this.pgBarConnection = new System.Windows.Forms.ProgressBar();
+            this.txt1QFILStatus = new System.Windows.Forms.TextBox();
+            this.btnQFIL = new System.Windows.Forms.Button();
+            this.btn1ConnectCheck = new System.Windows.Forms.Button();
+            this.txt1ConnectionStatus = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.bgWorkerConnection = new System.ComponentModel.BackgroundWorker();
+            this.bgWorkerQFIL = new System.ComponentModel.BackgroundWorker();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.tabControlMenu.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // tabControlMenu
+            // 
+            this.tabControlMenu.Controls.Add(this.tabPage1);
+            this.tabControlMenu.Controls.Add(this.tabPage2);
+            this.tabControlMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMenu.Location = new System.Drawing.Point(0, 0);
+            this.tabControlMenu.Name = "tabControlMenu";
+            this.tabControlMenu.SelectedIndex = 0;
+            this.tabControlMenu.Size = new System.Drawing.Size(800, 450);
+            this.tabControlMenu.TabIndex = 0;
+            this.tabControlMenu.SelectedIndexChanged += new System.EventHandler(this.tabControlMenu_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtLog);
+            this.tabPage1.Controls.Add(this.pgBarQFIL);
+            this.tabPage1.Controls.Add(this.pgBarConnection);
+            this.tabPage1.Controls.Add(this.txt1QFILStatus);
+            this.tabPage1.Controls.Add(this.btnQFIL);
+            this.tabPage1.Controls.Add(this.btn1ConnectCheck);
+            this.tabPage1.Controls.Add(this.txt1ConnectionStatus);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 422);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Step 1: 環境建立";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // pgBarQFIL
+            // 
+            this.pgBarQFIL.Location = new System.Drawing.Point(206, 88);
+            this.pgBarQFIL.Name = "pgBarQFIL";
+            this.pgBarQFIL.Size = new System.Drawing.Size(139, 23);
+            this.pgBarQFIL.Step = 1;
+            this.pgBarQFIL.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pgBarQFIL.TabIndex = 6;
+            // 
+            // pgBarConnection
+            // 
+            this.pgBarConnection.Location = new System.Drawing.Point(206, 41);
+            this.pgBarConnection.Name = "pgBarConnection";
+            this.pgBarConnection.Size = new System.Drawing.Size(139, 23);
+            this.pgBarConnection.Step = 1;
+            this.pgBarConnection.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pgBarConnection.TabIndex = 5;
+            // 
+            // txt1QFILStatus
+            // 
+            this.txt1QFILStatus.Location = new System.Drawing.Point(364, 89);
+            this.txt1QFILStatus.Name = "txt1QFILStatus";
+            this.txt1QFILStatus.ReadOnly = true;
+            this.txt1QFILStatus.Size = new System.Drawing.Size(240, 23);
+            this.txt1QFILStatus.TabIndex = 3;
+            // 
+            // btnQFIL
+            // 
+            this.btnQFIL.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btnQFIL.Enabled = false;
+            this.btnQFIL.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnQFIL.Location = new System.Drawing.Point(65, 89);
+            this.btnQFIL.Name = "btnQFIL";
+            this.btnQFIL.Size = new System.Drawing.Size(117, 23);
+            this.btnQFIL.TabIndex = 2;
+            this.btnQFIL.Text = "執行燒機";
+            this.btnQFIL.UseCompatibleTextRendering = true;
+            this.btnQFIL.UseVisualStyleBackColor = false;
+            this.btnQFIL.Click += new System.EventHandler(this.btnQFIL_Click);
+            // 
+            // btn1ConnectCheck
+            // 
+            this.btn1ConnectCheck.BackColor = System.Drawing.Color.Moccasin;
+            this.btn1ConnectCheck.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1ConnectCheck.Location = new System.Drawing.Point(65, 41);
+            this.btn1ConnectCheck.Name = "btn1ConnectCheck";
+            this.btn1ConnectCheck.Size = new System.Drawing.Size(117, 23);
+            this.btn1ConnectCheck.TabIndex = 1;
+            this.btn1ConnectCheck.Text = "確認攝影機狀態";
+            this.btn1ConnectCheck.UseVisualStyleBackColor = false;
+            this.btn1ConnectCheck.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt1ConnectionStatus
+            // 
+            this.txt1ConnectionStatus.Location = new System.Drawing.Point(364, 42);
+            this.txt1ConnectionStatus.Name = "txt1ConnectionStatus";
+            this.txt1ConnectionStatus.ReadOnly = true;
+            this.txt1ConnectionStatus.Size = new System.Drawing.Size(240, 23);
+            this.txt1ConnectionStatus.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 422);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // bgWorkerConnection
+            // 
+            this.bgWorkerConnection.WorkerReportsProgress = true;
+            this.bgWorkerConnection.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerConnection_DoWork);
+            this.bgWorkerConnection.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorkerConnection_ProgressChanged);
+            this.bgWorkerConnection.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorkerConnection_RunWorkerCompleted);
+            // 
+            // bgWorkerQFIL
+            // 
+            this.bgWorkerQFIL.WorkerReportsProgress = true;
+            this.bgWorkerQFIL.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorkerQFIL_DoWork);
+            this.bgWorkerQFIL.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorkerQFIL_ProgressChanged);
+            this.bgWorkerQFIL.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorkerQFIL_RunWorkerCompleted);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(206, 140);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(398, 189);
+            this.txtLog.TabIndex = 7;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.Controls.Add(this.tabControlMenu);
+            this.Name = "MainForm";
             this.Text = "OSSA tool";
+            this.tabControlMenu.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControlMenu;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txt1ConnectionStatus;
+        private System.Windows.Forms.Button btn1ConnectCheck;
+        private System.Windows.Forms.Button btnQFIL;
+        private System.Windows.Forms.TextBox txt1QFILStatus;
+        private System.ComponentModel.BackgroundWorker bgWorkerConnection;
+        private System.ComponentModel.BackgroundWorker bgWorkerQFIL;
+        private System.Windows.Forms.ProgressBar pgBarQFIL;
+        private System.Windows.Forms.ProgressBar pgBarConnection;
+        private System.Windows.Forms.TextBox txtLog;
     }
 }
 
--- ossaTool/MainForm.cs
+++ ossaTool/MainForm.cs
@@ -17,5 +17,110 @@ namespace ossaTool
             InitializeComponent();
         }
 
+        private String errorConnection = "連線失敗";
+        private String successConnection = "連線正常";
+        private String connecting = "連線中";
+        private String connectionLog = "狀態異常!\n請排除硬體問題再進行後續燒機";
+        private bool connectionStatus = false;
+
+        private String errorQFIL = "燒機失敗";
+        private String successQFIL = "燒機成功";
+        private String qfiling = "燒機中";
+        private String qfilLog = "狀態異常!\n請確認硬體是否異常\n請確認 USB/網路線是否脫落";
+        private bool qfilSuccessful = false;
+
+        private void tabControlMenu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt1ConnectionStatus.Text = connecting;
+            btn1ConnectCheck.Enabled = false;
+            bgWorkerConnection.RunWorkerAsync();
+        }
+        private void connectionProcess()
+        {
+            //todo add script here
+            for (int j = 0; j < 100; j++)
+            {
+                System.Threading.Thread.Sleep(10);
+                bgWorkerConnection.ReportProgress(j);
+            }
+            connectionStatus = true;
+        }
+
+        private void bgWorkerConnection_DoWork(object sender, DoWorkEventArgs e)
+        {
+            connectionProcess();
+        }
+
+        private void bgWorkerConnection_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgBarConnection.Value = e.ProgressPercentage;
+        }
+
+        private void bgWorkerConnection_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btn1ConnectCheck.Enabled = true;
+            if (connectionStatus)
+            {
+                txt1ConnectionStatus.Text = successConnection;
+                btnQFIL.Enabled = true;
+            }
+            else
+            {
+                txt1ConnectionStatus.Text = errorConnection;
+                txtLog.Text = connectionLog;
+            }
+        }
+        private void btnQFIL_Click(object sender, EventArgs e)
+        {
+            txt1QFILStatus.Text = qfiling;
+            btnQFIL.Enabled = false;
+            bgWorkerQFIL.RunWorkerAsync();
+        }
+
+        private void qFILProcess()
+        {
+            //todo add script here
+            for (int j = 0; j < 100; j++)
+            {
+                System.Threading.Thread.Sleep(5);
+                bgWorkerQFIL.ReportProgress(j);
+            }
+            qfilSuccessful = true;
+        }
+
+        private void bgWorkerQFIL_DoWork(object sender, DoWorkEventArgs e)
+        {
+            qFILProcess();
+        }
+
+        private void bgWorkerQFIL_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgBarQFIL.Value = e.ProgressPercentage;
+        }
+
+        private void bgWorkerQFIL_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnQFIL.Enabled = true;
+            if (qfilSuccessful)
+            {
+                txt1QFILStatus.Text = successQFIL;
+            }
+            else
+            {
+                txt1QFILStatus.Text = errorQFIL;
+                txtLog.Text = qfilLog;
+            }
+        }
+
     }
 }
